44c9974d1f5a3d0af352e733ad542f7edaaf9b31
Amulya Parmar! TYG!! <parmar.amulya@gmail.com>
2026-09-03 14:54:13 -0400

    Set Ivy Cap Revery proposal path

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/revery-starkville",
+        source: "/ivy-cap/revery-starkville",
         destination: "/proposals/ivy-cap/index.html",
       },
       {
@@ -45,12 +45,17 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/proposals/ivy-cap",
-        destination: "/revery-starkville",
+        destination: "/ivy-cap/revery-starkville",
         permanent: true,
       },
       {
         source: "/ivy-cap/revery-starkville-ms",
-        destination: "/revery-starkville",
+        destination: "/ivy-cap/revery-starkville",
+        permanent: true,
+      },
+      {
+        source: "/revery-starkville",
+        destination: "/ivy-cap/revery-starkville",
         permanent: true,
       },
       {
